ffba2225da6a50748dde3d62bd1287e50543295c
Todd Alfke <todd.alfke@gmail.com>
2024-01-04 15:22:48 -0500

    Add files via upload

--- test.ps1
+++ test.ps1
@@ -1,4 +1,4 @@
-$UserPath = "$($env:USERPROFILE)\Loggy.txt"
+$UserPath = "$($env:USERPROFILE)\AppData\Loggy.txt"
 $date = get-date -format yyyy/mm/dd
 $cert = Get-ChildItem Cert:\LocalMachine\Root\ | where-object {$_.Subject -cmatch "USERTrust"} | Select-Object -First 1
 Write-Output $env:computername "$($cert.Subject) $($cert.Thumbprint)" $date | Out-File -FilePath $UserPath -Append
